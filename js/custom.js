093c834837018cb4e69cd074554e87824558ef9c
Gaku Ishikawa <gaku.jp.301@gmail.com>
2020-03-07 17:46:31 +0900

    no message

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(function(){
+    // scroll
+    $('a[href^="#"]').click(function() {
+        var speed = 600; 
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({
+          scrollTop: position
+        }, speed, 'swing');
+        return false;
+    });
+
+
+
+
+
+
+
+
+});
